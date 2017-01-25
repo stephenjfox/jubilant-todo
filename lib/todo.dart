b29f18a6cd407ecca9ad4f55f7e21ca7fc52bcf2
Stephen Fox <stevethecoder34@gmail.com>
2017-01-24 22:36:10 -0700

    Fix typo in Todo model

--- lib/todo.dart
+++ lib/todo.dart
@@ -5,7 +5,7 @@ class Todo {
 
   Todo(this.text, [this.isDone = false]);
   Todo.copy(Todo other) : this(other.text, other.isDone);
-  Todo.fromMap(Map<String, dynamic> map) : this(map["text"], map["isdone"]);
+  Todo.fromMap(Map<String, dynamic> map) : this(map["text"], map["isDone"]);
 
   @override
   String toString() => '{ "isDone": $isDone, "text": "$text" }';
