1f165ef30f0c883701a9914f63bf1e6f3893e198
Stephen Fox <stevethecoder34@gmail.com>
2017-01-24 23:07:45 -0700

    Complete barebones implementation
    Playground shows that serialization via .toString() works.
    
    Simple implementation for a simple requirement.
    
    Being stateless in Dart is kind of hard, but I'll find a way!

--- example/playground.dart
+++ example/playground.dart
@@ -2,7 +2,15 @@
 
 import 'package:jubilant_todo/todo.dart';
 
+import 'dart:convert';
+
 main(List<String> args) {
   var todo = new Todo("test");
   print(todo);
+
+  var decoded = JSON.decode(todo.toString());
+  print("Decoded JSON: type - ${decoded.runtimeType}, toString() - ${decoded.toString()}");
+
+  var otherTodo = new Todo.fromMap(decoded);
+  print(otherTodo);
 }
--- web/main.dart
+++ web/main.dart
@@ -1,4 +1,31 @@
-void main(List<String> args) {
+import 'dart:html';
+
+import 'package:jubilant_todo/todo.dart';
+
+InputElement _todoInput;
+UListElement _todoList;
+
+void main() {
   // This is the entrypoint, so I can do most of the primative coding
   // for the app right in this file
+  _todoInput = querySelector("#todo-input");
+  _todoList = querySelector("#todo-list");
+
+  _todoInput.onChange.listen((e) {
+    final inputText = e.currentTarget.value; // not IntelliSense'd
+
+    // make Todo and clear input
+    var newTodo = new Todo(inputText);
+    _todoInput.value = '';
+
+    // make representative li
+    LIElement nextTodoItem = visualize(todo: newTodo);
+
+    // append ul with representation
+    _todoList.append(nextTodoItem);
+  });
+}
+
+LIElement visualize({Todo todo}) {
+  return new LIElement()..text = todo.text;
 }
