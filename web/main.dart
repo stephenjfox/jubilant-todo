199f50120c643b97a0aa99934e2ad2b6c1280597
Stephen Fox <stevethecoder34@gmail.com>
2017-01-24 23:47:29 -0700

    Add basic styling
    Centering can be done without flex-box
      Just don't over complicate it.
    
    I'm still on my quest for a good way to write
      CSS.
    
    Dart is so simple and expressive.

--- web/main.dart
+++ web/main.dart
@@ -2,30 +2,31 @@ import 'dart:html';
 
 import 'package:jubilant_todo/todo.dart';
 
-InputElement _todoInput;
-UListElement _todoList;
+InputElement todoInput;
+UListElement todoList;
 
 void main() {
   // This is the entrypoint, so I can do most of the primative coding
   // for the app right in this file
-  _todoInput = querySelector("#todo-input");
-  _todoList = querySelector("#todo-list");
+  todoInput = querySelector("#todo-input");
+  todoList = querySelector("#todo-list");
 
-  _todoInput.onChange.listen((e) {
+  todoInput.onChange.listen((e) {
     final inputText = e.currentTarget.value; // not IntelliSense'd
 
     // make Todo and clear input
-    var newTodo = new Todo(inputText);
-    _todoInput.value = '';
+    var newTodo = new Todo(inputText)..isDone = true;
+    todoInput.value = '';
 
     // make representative li
     LIElement nextTodoItem = visualize(todo: newTodo);
 
     // append ul with representation
-    _todoList.append(nextTodoItem);
+    todoList.append(nextTodoItem);
   });
 }
 
 LIElement visualize({Todo todo}) {
+  // TODO: read the and modify the existing LIElement. Don't make a new one
   return new LIElement()..text = todo.text;
 }
