286b913fd05729e322efd9ed7cfa6faa71f506b6
Stephen Fox <stevethecoder34@gmail.com>
2017-01-30 21:57:54 -0700

    Use the classic MVC model to handle UI

--- web/main.dart
+++ web/main.dart
@@ -8,29 +8,44 @@ UListElement todoList;
 void main() {
   // This is the entrypoint, so I can do most of the primative coding
   // for the app right in this file
-  todoInput = querySelector("#todo-input");
-  todoList = querySelector("#todo-list");
+  todoInput = querySelector('#todo-input');
+  todoList = querySelector('#todo-list');
 
   todoInput.onChange.listen((e) {
-    final inputText = e.currentTarget.value; // not IntelliSense'd
+    final userInput = e.currentTarget.value; // not IntelliSense'd
 
-    // make Todo and clear input
-    var newTodo = new Todo(inputText)..isDone = true;
+    // make model-view-controller respresentation
+    TodoController nextTodoItem = visualize(todoText: userInput);
+
     todoInput.value = '';
 
-    // make representative li
-    LIElement nextTodoItem = visualize(todo: newTodo);
-
     // append ul with representation
-    todoList.append(nextTodoItem);
+    todoList.append(nextTodoItem.view);
   });
 }
 
-LIElement visualize({Todo todo}) {
-  // TODO: read the and modify the existing LIElement. Don't make a new one
-  return new LIElement()
-    ..text = todo.text
-    ..onClick.listen((e) {
-      (e.currentTarget as LIElement).classes.toggle('completed-todo');
-    });
+/// This method takes a "primitive" Todo instance and converts it into a
+/// proper model-view-controller:
+/// Model - Todo instance
+/// View - LIElement that will be presented
+/// Controller - TBD; listens on the event of the
+TodoController visualize({String todoText}) {
+  final Todo newTodo = new Todo(todoText);
+
+  return new TodoController(newTodo, new LIElement()..text = newTodo.text);
+}
+
+class TodoController {
+  final Todo todo;
+  final LIElement view;
+
+  TodoController(this.todo, this.view) {
+    view.onClick.listen(handleClick);
+  }
+
+  void handleClick(MouseEvent event) {
+    view.classes.toggle('completed-todo'); // toggle the view
+    todo.isDone = !todo.isDone; // toggle the backing data
+    print('handleClick - $todo');
+  }
 }
